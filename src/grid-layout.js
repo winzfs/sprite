41c272d5411d8e2968e7d4b4c0cf9370d762806a
winzfs <jazzhjm@gmail.com>
2026-06-03 13:02:06 +0900

    Load modern texture sound engine

--- src/grid-layout.js
+++ src/grid-layout.js
@@ -55,7 +55,8 @@ function loadMediaToolHelpers() {
     .then(() => loadScriptOnce('src/pixel-art-unified-modes.js?v=2', 'pixel-art-unified-modes'))
     .then(() => loadScriptOnce('src/pixel-art-floating-controls.js?v=5', 'pixel-art-floating-controls'))
     .then(() => loadScriptOnce('src/sfx-maker.js?v=1', 'sfx-maker'))
-    .then(() => loadScriptOnce('src/sfx-premium-engine.js?v=1', 'sfx-premium-engine'));
+    .then(() => loadScriptOnce('src/sfx-premium-engine.js?v=1', 'sfx-premium-engine'))
+    .then(() => loadScriptOnce('src/sfx-modern-texture-engine.js?v=1', 'sfx-modern-texture-engine'));
 }
 
 function bindFixedFrameGrids() {
