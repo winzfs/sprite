ab6500f22a3720bb3177250aebfc07f6f409305c
winzfs <jazzhjm@gmail.com>
2026-06-03 13:12:40 +0900

    Load Pro SFX studio UI

--- src/grid-layout.js
+++ src/grid-layout.js
@@ -55,8 +55,7 @@ function loadMediaToolHelpers() {
     .then(() => loadScriptOnce('src/pixel-art-unified-modes.js?v=2', 'pixel-art-unified-modes'))
     .then(() => loadScriptOnce('src/pixel-art-floating-controls.js?v=5', 'pixel-art-floating-controls'))
     .then(() => loadScriptOnce('src/sfx-maker.js?v=1', 'sfx-maker'))
-    .then(() => loadScriptOnce('src/sfx-premium-engine.js?v=1', 'sfx-premium-engine'))
-    .then(() => loadScriptOnce('src/sfx-modern-texture-engine.js?v=1', 'sfx-modern-texture-engine'));
+    .then(() => loadScriptOnce('src/sfx-pro-studio.js?v=1', 'sfx-pro-studio'));
 }
 
 function bindFixedFrameGrids() {
