51da06c303af9031dc1af248e32fe03138fe421a
winzfs <jazzhjm@gmail.com>
2026-06-03 13:41:36 +0900

    Load expanded SFX waveform bank

--- src/grid-layout.js
+++ src/grid-layout.js
@@ -56,7 +56,9 @@ function loadMediaToolHelpers() {
     .then(() => loadScriptOnce('src/pixel-art-floating-controls.js?v=5', 'pixel-art-floating-controls'))
     .then(() => loadScriptOnce('src/sfx-maker.js?v=1', 'sfx-maker'))
     .then(() => loadScriptOnce('src/sfx-pro-studio.js?v=1', 'sfx-pro-studio'))
-    .then(() => loadScriptOnce('src/sfx-mastering-studio.js?v=1', 'sfx-mastering-studio'));
+    .then(() => loadScriptOnce('src/sfx-mastering-studio.js?v=1', 'sfx-mastering-studio'))
+    .then(() => loadScriptOnce('src/sfx-waveform-bank.js?v=1', 'sfx-waveform-bank'))
+    .then(() => loadScriptOnce('src/sfx-waveform-engine.js?v=1', 'sfx-waveform-engine'));
 }
 
 function bindFixedFrameGrids() {
